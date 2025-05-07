3e077c0b87a7e6fa7adb5bc89925a40093d96323
adesh9201 <panditadesh123@gmail.com>
2025-05-07 12:15:02 +0530

    update link

--- ImageVault-backend/server.js
+++ ImageVault-backend/server.js
@@ -8,7 +8,10 @@ const path = require("path");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://atadeshmishra-imagevault.onrender.com', // replace with actual Vercel URL
+    credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/images", imageRoutes);
